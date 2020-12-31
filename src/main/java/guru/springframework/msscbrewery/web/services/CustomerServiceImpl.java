464a1f8afb77964546116a913e8dd00b748a77a5
JJangseong <sungjin5891@gmail.com>
2020-12-31 19:06:59 +0900

    create customer

---
+++ src/main/java/guru/springframework/msscbrewery/web/services/CustomerServiceImpl.java
@@ -0,0 +1,34 @@
+package guru.springframework.msscbrewery.web.services;
+
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public CustomerDto getCustomerById(UUID customerId) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .name("Joe Buck")
+                .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+       // TODO: Updating a Customer
+    }
+
+    @Override
+    public void deleteCustomer(UUID customerId) {
+
+    }
+}
